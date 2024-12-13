fdd336c66041f3e66f1251e6399de15fcdf25a1f
naimdeb <naimdebard02@gmail.com>
2024-12-13 14:45:38 +0100

    c bon

--- index.php
+++ index.php
@@ -10,62 +10,47 @@
   <link rel="stylesheet" href="./css/style.css">
 </head>
 
-<body class="h-[100vh] bg-body">
+<body>
+<div id="app">
+  <div id="star-container">
 
+  <header class="flex w-100 justify-around pt-5 items-center opacity-[1] z-2">
 
-  <!-- Header Quiz404 -->
-  <header class="flex w-100 justify-around pt-5 items-center opacity-[1]">
-
-    <div class="w-[9%] h-[70px] shadow-[0_20px_17px_4px_rgba(0,0,0,0.99)] items-center flex justify-center bg-gradient-to-r from-amber-300 to-yellow-600">
-      <a class="text-white font-first-font font-extrabold" href="formulaire.php">Inscription</a>
+    
+    <a class="text-white font-first-font font-extrabold" href="formulaire.php">
+    <div class="w-[9%] h-[70px] items-center flex justify-center btn">
+    Inscription
     </div>
+    </a>
 
-
-    <div class="w-[50%] shadow-[0_20px_17px_4px_rgba(0,0,0,0.99)] p-5 items-center text-center bg-gradient-to-r from-amber-300 to-yellow-600">
+    <div class="w-[50%] p-5 items-center text-center ">
       <h2 class="text-8xl font-extrabold font-first-font">
         <span>
           < 
         </span>
         <span>Quiz</span>
-        <span class="text-purple-500">404</span>
+        <span class="text-[#8344bc]">405</span>
         <span>/></span>
       </h2>
     </div>
-    <div class="w-[9%] h-[70px] shadow-[0_20px_17px_4px_rgba(0,0,0,0.99)] items-center flex justify-center bg-gradient-to-r from-amber-300 to-yellow-600">
+    <div class="w-[9%] h-[70px] items-center flex justify-center ">
       <a class="text-white font-first-font font-extrabold" href="#">Connexion</a>
     </div>
   </header>
-
-
-
-
-  <!-- Main -->
-  <main class="mt-8 flex justify-center">
-
-
-
-      <!-- Formulaire d'inscription -->
-      <form action="./process/process_create_user.php" method="post">
-
-      
-
-      </form>
-
-    </div>
-
-  </main>
-
   
+<main>
 
-    <!-- Background animation -->
-    <div class="background">
-      <!-- étoiles pattern -->
-      <div class="w-[40%] max-h-fit shadow-[0_20px_17px_4px_rgba(0,0,0,0.99)] items-center flex justify-center bg-gradient-to-r from-amber-300 to-yellow-600">
+    <div id="star-pattern"></div>
+    <div id="star-gradient-overlay">
+      
     </div>
-
-
-
   </div>
+  <div id="stripe-container">
+    <div id="stripe-pattern"></div>
+  </div>
+</main>
+
+
 </body>
 
 </html>
