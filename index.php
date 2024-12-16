4f01f2c37a602546379d5519f9d268e9272b3c01
Karl2242 <kchikhicloud@gmail.com>
2024-12-16 09:33:12 +0100

    ADD: index page

--- index.php
+++ index.php
@@ -33,9 +33,13 @@
         <span>/></span>
       </h2>
     </div>
-    <div class="w-[9%] h-[70px] items-center flex justify-center ">
-      <a class="text-white font-first-font font-extrabold" href="#">Connexion</a>
+ 
+      
+    <a class="text-white font-first-font font-extrabold" href="formulaire.php">
+    <div class="w-[9%] h-[70px] items-center flex justify-center btn">
+    Connexion
     </div>
+    </a>
   </header>
   
 <main>
@@ -46,7 +50,25 @@
     </div>
   </div>
   <div id="stripe-container">
-    <div id="stripe-pattern"></div>
+    <div id="stripe-pattern">
+<div class="h-[100%] pt-[250px]">
+  <div class="flex flex-col gap-9 text-center w-100 items-center">   
+  <h1 class="font-first-font text-5xl">Bienvenue sur QUIZ405 inscrivez vous ou connecter vous pour jouer !</h1>
+  <div class="flex gap-4 w-full justify-center text-center">
+  <a class="text-white font-first-font font-extrabold hover:scale-110 transition-all" href="formulaire.php">
+    <div class="w-[9%] h-[70px] items-center flex justify-center btn">
+    Inscription
+    </div>
+    </a>
+    <a class="text-white font-first-font font-extrabold hover:scale-110 transition-all" href="formulaire.php">
+    <div class="w-[9%] h-[70px] items-center flex justify-center btn">
+    Connexion
+    </div>
+    </a>
+  </div>
+  </div>
+</div>
+    </div>
   </div>
 </main>
 
