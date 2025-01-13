5248026ddb3dc83af1ba819cdae409c7ef0fa577
naimdeb <naimdebard02@gmail.com>
2025-01-13 11:35:09 +0100

    changed autoloader

--- utils/autoloader.php
+++ utils/autoloader.php
@@ -1,9 +1,32 @@
 <?php
 
 spl_autoload_register(function ($className) {
-    $baseDir = __DIR__. '/../src/Entities/';
-    $file = $baseDir . str_replace('\\', '/', $className) . '.php';
+    // Base directory (src)
+    $baseDir = __DIR__ . '/../src/';
+    
+    // Déterminer le répertoire en fonction du suffixe du nom de la classe
+    switch (true) {
+        case substr($className, -10) === 'Repository':
+            $directory = 'Repositories';
+            break;
+        case substr($className, -7) === 'Manager':
+            $directory = 'Managers';
+            break;
+        case substr($className, -6) === 'Mapper':
+            $directory = 'Mappers';
+            break;
+        case substr($className, -10) === 'Controller':
+            $directory = 'Controllers';
+            break;
+        default:
+            $directory = 'Entities';
+            break;
+    }
 
+    // Construire le chemin complet du fichier
+    $file = $baseDir . $directory . '/' . $className . '.php';
+
+    // Charge le fichier si trouvé
     if (file_exists($file)) {
         require $file;
     }
