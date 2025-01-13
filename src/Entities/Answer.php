ef59bed213e38b7bccd725ae994c7abf5b6533c2
naimdeb <naimdebard02@gmail.com>
2025-01-13 12:24:30 +0100

    add answer

--- src/Entities/Answer.php
+++ src/Entities/Answer.php
@@ -1,7 +1,30 @@
 <?php 
 
 class Answer{
-    
+
+    private string $intitule;
+    private bool $isRightAnswer;
+
+
+    public function __construct(string $intitule ,bool $isRightAnswer = false)
+    {
+        $this->intitule = $intitule;
+        $this->isRightAnswer = $isRightAnswer;
+        
+    }
+
+
+    // Getters
+
+    public function getIntitule() : string{
+        return $this->intitule;
+    }
+    public function getisRightAnswer() : bool{
+        return $this->isRightAnswer;
+    }
+
+
+
 }
 
 
