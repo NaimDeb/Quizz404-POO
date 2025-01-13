e3eb40919b6cc38ce496cb998ec47bacc157e328
naimdevweb <naimhamidi19@gmal.com>
2025-01-13 12:13:16 +0100

    QCM.php

---
+++ src/Entities/QCM.php
@@ -0,0 +1,36 @@
+<?php
+
+class QCM {
+private int $id;
+private array $questions;
+private string $nom;
+
+	public function __construct($id, $questions, $nom) {
+		$this->id = $id;
+		$this->questions = [];
+		$this->nom = $nom;
+	}
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getQuestion(): array {
+        return $this->questions;
+    }
+
+    public function setQuestion(array $questions): self {
+        foreach($questions as $question){
+            if(!$question instanceof QCM){
+                throw new Exception("Il faut que le tableau soit composé de QCM uniquement");
+            }
+        }
+        $this->questions = $questions;
+        return $this;
+    }
+
+    public function getNom(): string {
+        return $this->nom;
+    }
+}
+?>
