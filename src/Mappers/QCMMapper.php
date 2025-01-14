b0725798aef4a4025320eda3628fa47f4f4f4e60
naimdeb <naimdebard02@gmail.com>
2025-01-14 09:38:10 +0100

    reworked QCMMAPPer

--- src/Mappers/QCMMapper.php
+++ src/Mappers/QCMMapper.php
@@ -3,32 +3,9 @@
 class QcmMapper {
     private PDO $db;
 
-    
-    public function __construct(PDO $db) {
-        $this->db = $db;
-    }
 
-    // Méthode pour récupérer un QCM complet avec ses questions et réponses
-    public function getQcmById(int $qcmId): ?QCM {
-       
-        $stmt = $this->db->prepare("SELECT * FROM quiz WHERE id = :id");
-        $stmt->execute(['id' => $qcmId]);
-        $qcmData = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if (!$qcmData) {
-            return null; 
-        }
-
-        // Créer l'objet QCM
-        $qcm = new QCM();
-        $qcm->getId($qcmData['id']);
-        $qcm->getNom($qcmData['nom']);
-
-        
-        $questions = $this->getQuestionsByQcmId($qcmId);
-        $qcm->setQuestion($questions);
-
-        return $qcm;
+    public function mapToObject(array $data): QCM {
+        return new QCM($data["id"], $data["titre"]);
     }
 
     
