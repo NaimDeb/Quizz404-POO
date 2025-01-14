7586a4120baec913f24c1f70e51e4f5f056505db
naimdeb <naimdebard02@gmail.com>
2025-01-14 16:02:36 +0100

    fixed test

--- public/test.php
+++ public/test.php
@@ -30,7 +30,7 @@ $qcm1->setQuestion($questions);
 include_once "./assets/components/htmlstart.php"
 ?>
 <?php
-echo QcmManager::generateDisplayIndividualQuizz(1);
+echo QcmManager::generateDisplayIndividualQuizz(1,$pdo);
 
 ?>
 
